e70e168cf2ac2fb158eace593ba0d3711d482fe8
Kristi Green <kngreen40@gmail.com>
2017-02-07 16:34:35 -0700

    Modify Scripts file Modify Styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,63 @@
+///functions/////
+
+///Create bookmark w/user input
+
+
+
+
+
+
+///Mark as "read"
+
+
+
+
+
+
+////Remove Link from page///
+
+
+
+
+
+////Delete URl//////
+
+
+
+
+
+///Disable links//////
+
+
+
+
+
+
+
+///Keep count of links on page////
+
+
+
+
+
+
+
+///Count of read and unread links on page///
+
+
+
+
+
+
+
+
+
+
+
+
 //input event listener//
-$('#enter-button').on('click',function(e){
-	console.log(e.target);
-	$('#web-title, #web-url')
+$('#input-fields').on('click',function(e){
+	$webTitle =
+	$(this.#web-title).val();
+	$(this.#web-url).val();
 });
