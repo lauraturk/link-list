f487e33f945f8e43d8e8af0b03c20d57966726a3
Kristi Green <kngreen40@gmail.com>
2017-02-08 13:59:12 -0700

    Fix styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -25,7 +25,7 @@ $('#enter-button').on('click', function(e){
 	checkFields();
 	$('#side-bar').append('<article><h2 class="bm-title">' + $('#web-title').val() + '</h2><hr><p class="bm-url"><a href="#">' + $('#web-url').val() + '</a></p><hr><button class="read-button">Read</button><button class="delete-button">Delete</button></article>');
 	var n = $('article').length;
-	$('#saved-links').text(n + ' Links Saved');
+	$('#saved-links').text(n + ' Saved Links ');
 		});
 
 
