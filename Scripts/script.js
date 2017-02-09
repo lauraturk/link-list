4d817c4ed98e7c52555ce5d8d6cd71f66df6dc2e
Laura Turk <laura.lk.turk@gmail.com>
2017-02-08 19:44:11 -0700

    Redo Read counter

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,9 +5,8 @@ function checkFields (){
 	$('#input-fields').on('keyup', function(){
 		if($('#web-title').val() === '' || ($('#web-url').val() === '')){
 				$('form input[type="submit"]').prop('disabled',true);
-				console.log("blueb")
 		}else{
-					$('form input[type="submit"]').prop('disabled',false);
+				$('form input[type="submit"]').prop('disabled',false);
 		}
 	})
 }
@@ -22,9 +21,9 @@ checkFields()
 $('#enter-button').on('click', function(e){
 	e.preventDefault()
 	// checkFields()
-	$('#side-bar').append('<article><h2 class="bm-title">' + $('#web-title').val() + '</h2><p class="bm-url"><a href="#">' + $('#web-url').val() + '</a></p><button class="read-button">Read</button><button class="delete-button">Delete</button></article>');
+	$('#side-bar').append('<article><h2 class="bm-title">' + 			$('#web-title').val() + '</h2><p class="bm-url"><a href="#">' + $('#web-url').val() + '</a></p><button 	class="read-button">Read</button><button 	class="delete-button">Delete</button></article>');
 	var n = $('article').length;
-	$('#saved-links').text(n + ' Saved Links ');
+	$('#saved-links').text(n + ' Saved');
 });
 
 
@@ -33,8 +32,9 @@ $('#enter-button').on('click', function(e){
 ///Mark as "read"
 $('#side-bar').on('click', '.read-button', function(){
 	$(this).toggleClass('read');
-	// var targetBtn = $(event.target);
-	// targetBtn.toggleClass('.read');
+	var n = $('.read').length;
+	$('#read-links').text(n + ' Read');
+
 });
 
 
@@ -50,7 +50,8 @@ $('#side-bar').on('click', '.read-button', function(){
 ////Delete URl//////
 $('#side-bar').on('click', '.delete-button',function() {
 	  $(this).parent().remove();
-
+		var n = $('article').length;
+		$('#saved-links').text(n + ' Saved');
 });
 
 
