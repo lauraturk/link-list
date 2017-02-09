afde6b96df365b2fb71903d4fbea7c619d156ef7
Laura Turk <laura.lk.turk@gmail.com>
2017-02-08 21:48:09 -0700

    Add Flexbox

--- Scripts/script.js
+++ Scripts/script.js
@@ -20,7 +20,6 @@ checkFields()
 ///Create bookmark w/user input
 $('#enter-button').on('click', function(e){
 	e.preventDefault()
-	// checkFields()
 	$('#side-bar').append('<article><h2 class="bm-title">' + 			$('#web-title').val() + '</h2><p class="bm-url"><a href="#">' + $('#web-url').val() + '</a></p><button 	class="read-button">Read</button><button 	class="delete-button">Delete</button></article>');
 	var n = $('article').length;
 	$('#saved-links').text(n + ' Saved');
